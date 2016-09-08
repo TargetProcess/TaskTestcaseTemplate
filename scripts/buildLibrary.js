7dc6e21a8733a034f477e77b1241f983d3f07269
Victor Homyakov <vkhomyackov@gmail.com>
2016-09-08 19:09:36 +0300

    Make build scripts cross-platform; update ESLint

---
+++ scripts/buildLibrary.js
@@ -0,0 +1,6 @@
+﻿const shell = require('shelljs');
+const outputPath = 'build/library';
+shell.rm('-rf', outputPath);
+shell.exec(`webpack --config webpack-library.config.js --output-path ${outputPath}`);
+shell.cp('docs/README.md', `${outputPath}/README.mkd`);
+shell.cp('docs/*.png', `${outputPath}/`);
